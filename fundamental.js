ed6d29a03a6b3845cc0018fea7bdfb476aa8511b
ismail-dev-code <hm.ismail772@gmail.com>
2025-11-05 18:27:22 +0600

    level-2 upgrading

---
+++ fundamental.js
@@ -0,0 +1,22 @@
+const onekData = [
+  {
+    name: "abul",
+    age: 2,
+    rating: 4,
+    language: "english",
+  },
+  {
+    name: "kamal",
+    age: 4,
+    rating: 2,
+    language: "bengali",
+  },
+  {
+    name: "jamal",
+    age: 7,
+    rating: 4,
+    language: "hindi",
+  },
+];
+
+console.log(onekData[2].age);
